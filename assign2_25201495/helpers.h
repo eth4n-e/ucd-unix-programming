d927f71ff4a0f805e881fc7e937674a49c945fd6
eth4n-e <etep9497@colorado.edu>
2025-03-23 17:30:33 +0000

    made distribution

--- assign2_25201495/helpers.h
+++ assign2_25201495/helpers.h
@@ -2,6 +2,12 @@
 #define HELPERS_H
 #include <stddef.h>
 
+/*
+Name: Ethan Epperson
+Student Number: 25201495
+Email: ethan.epperson@ucdconnect.ie
+*/
+
 void display_prompt();
 struct tm* get_current_time();
 char** parse_and_store_tokens(char *line);
--- assign2_25201495/signal_handlers.h
+++ assign2_25201495/signal_handlers.h
@@ -1,6 +1,12 @@
 #ifndef SIGNAL_HANDLERS_H
 #define SIGNAL_HANDLERS_H
 
+/*
+Name: Ethan Epperson
+Student Number: 25201495
+Email: ethan.epperson@ucdconnect.ie
+*/
+
 void handle_sigint(int sig);
 void setup_signal_handlers();
 
