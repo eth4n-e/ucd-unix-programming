fcfe8b4c972da6bb142379b47e8a34ca46f08e6a
eth4n-e <etep9497@colorado.edu>
2025-04-22 16:24:34 +0100

    starting on assignment 4 bash script for transcript

---
+++ assign4_25201495/bsctranscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source ./assignment4-data/stage1.sh.inc
+source ./assignment4-data/stage2.sh.inc
+source ./assignment4-data/stage3.sh.inc
+source ./assignment4-data/stage4.sh.inc
+source ./assignment4-data/grades.sh.inc
+source ./assignment4-data/mincredits.sh.inc
+
+# Notes:
+# Script takes on argument
+# If no args, invalid args, or --help provided, must print correct usage
+# if grade F is selected, pick another elective 
